9b22b561153127af84377f56fb0463da39e62ec4
ChristinaW <97368143+polly89@users.noreply.github.com>
2022-08-25 06:27:05 -0400

    Add files via upload

---
+++ varsAndConditionals.js
@@ -0,0 +1,37 @@
+let jonSnowAttack = 25
+let jamieLannisterAttack = 35
+
+let jonSnowHealth = 100
+let jonSnowDefense = 10
+
+// if(jonSnowAttack > jamieLannisterAttack){
+//     console.log('Jon Snow has a greater attack power than Jamie Lannister.')
+// } else if(jamieLannisterAttack > jonSnowAttack){
+//     console.log("Jamie Lannister has a greater attack power than Jon Snow.")
+// } else {
+//     console.log('Jon and Jamie are the samie.')
+// }
+
+
+// if(jonSnowHealth <= jamieLannisterAttack - jonSnowDefense){
+//     console.log("Jon does not have enough health to withstand the attack.")
+// } else {
+//     jonSnowHealth -= (jamieLannisterAttack - jonSnowDefense)
+//     console.log(`Jon has ${jonSnowHealth} health.`)
+// }
+
+// if(jonSnowHealth + 50 >= 100){
+//     jonSnowHealth = 100
+//     console.log('Jon is back to full health!')
+// } else {
+//     jonSnowHealth += 50
+//     console.log(`John's health has increased to ${jonSnowHealth}.`)
+// }
+
+let coinLandsHeads = false
+
+if(coinLandsHeads === true){
+    console.log('Cut off his head!')
+} else {
+    console.log('Let him live.')
+}
